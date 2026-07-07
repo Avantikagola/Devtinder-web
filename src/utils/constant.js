d78aed5ff90059147df61dbef702bd0a07c7e956
Avantika Gola <avantika.gola05@gmail.com>
2026-07-07 13:23:22 +0530

    Update backend URL for production

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,4 @@
-//production
-
-//export const BASE_URL = "/api";
-
-//localhost
-
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7777" :" /api";
+export const BASE_URL =
+  location.hostname === "localhost"
+    ? "http://localhost:7777"
+    : "https://devtinder-0esl.onrender.com";
